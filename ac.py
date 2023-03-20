a0af8a86d17f9785b2683bf272e6ceb11b4cc832
Kirby Rs <andkirby@gmail.com>
2023-03-20 03:31:01 +0200

    Added source/result files.

---
+++ ac.py
@@ -0,0 +1,219 @@
+def getCarState(car_identifier, info_identifier, optional_identifier):
+    pass
+
+def log(value):
+    pass
+
+def console(value):
+    pass
+
+def newApp(value):
+    pass
+
+def setTitle(controlo_identifier, title):
+    pass
+
+def setSize(controlo_identifier, width, height):
+    pass
+
+def addLabel(control_identifier, value):
+    pass
+
+def setPosition(controlo_identifier, x, y):
+    pass
+
+def setIconPosition(controlo_identifier, x, y):
+    pass
+
+def setTitlePosition(controlo_identifier, x, y):
+    pass
+
+def getPosition(controlo_identifier):
+    pass
+
+def setText(control_identifier, value):
+    pass
+
+def getText(controlo_identifier):
+    pass
+
+def setBackgroundOpacity(control_identifier, value):
+    pass
+
+def drawBackground(control_identifier, value):
+    pass
+
+def drawBorder(control_identifier, value):
+    pass
+
+def setBackgroundTexture(control_identifier, path):
+    pass
+
+def setFontAlignment(control_identifier, alignment):
+    pass
+
+def setBackgroundColor(control_identifier, r, g, b):
+    pass
+
+def setVisible(control_identifier, value):
+    pass
+
+def addOnAppActivatedListener(control_identifier, value):
+    pass
+
+def addOnAppDismissedListener(control_identifier, value):
+    pass
+
+def addRenderCallback(control_identifier, value):
+    pass
+
+def setFontColor(control_identifier, r, g, b, a):
+    pass
+
+def addButton(control_identifier, value):
+    pass
+
+def addOnClickedListener(control_identifier, value):
+    pass
+
+def addGraph(control_identifier, value):
+    pass
+
+def addSerieToGraph(control_identifier, r, g, b):
+    pass
+
+def addValueToGraph(control_identifier, serie_index, value):
+    pass
+
+def setRange(control_identifier, min_value, max_value):
+    pass
+
+def addSpinner(control_identifier, value):
+    pass
+
+def setValue(control_identifier, value):
+    pass
+
+def getValue(control_identifier):
+    pass
+
+def setStep(control_identifier, value):
+    pass
+
+def addOnValueChangeListener(control_identifier, value):
+    pass
+
+def addProgressBar(control_identifier, value):
+    pass
+
+def addInputText(control_identifier, value):
+    pass
+
+def setFocus(control_identifier, focus):
+    pass
+
+def addOnValidateListener(control_identifier, value):
+    pass
+
+def addListBox(control_identifier, name):
+    pass
+
+def addItem(control_identifier, name):
+    pass
+
+def removeItem(control_identifier, id):
+    pass
+
+def getItemCount(control_identifier):
+    pass
+
+def setItemNumberPerPage(control_identifier, number):
+    pass
+
+def highlightListBoxItem(control_identifier, id):
+    pass
+
+def addOnListBoxSelectionListener(control_identifier, value):
+    pass
+
+def addOnListBoxDeselectionListener(control_identifier, value):
+    pass
+
+def setAllowDeselection(control_identifier, allow_deselection):
+    pass
+
+def setAllowMultiSelection(control_identifier, allow_multi_selection):
+    pass
+
+def getSelectedItems(control_identifier):
+    pass
+
+def addCheckBox(control_identifier, value):
+    pass
+
+def addOnCheckBoxChanged(control_identifier, value):
+    pass
+
+def isAcLive():
+    pass
+
+def restart():
+    pass
+
+def isCarInPitlane():
+    pass
+
+def getCarSkin(car_id):
+    pass
+
+def getDriverNationCode(car_id):
+    pass
+
+def getCurrentSplits(car_id):
+    pass
+
+def getTrackLength(car_id):
+    pass
+
+def getWindSpeed():
+    pass
+
+def getWindDirection():
+    pass
+
+def isAIControlled():
+    pass
+
+def getCarEngineBrakeCount():
+    pass
+
+def getCarPowerControllerCount():
+    pass
+
+def freeCameraSetClearColor(r, g, b, alpha):
+    pass
+
+def freeCameraMoveForward(float):
+    pass
+
+def freeCameraMoveRight(float):
+    pass
+
+def freeCameraMoveUpWorld(float):
+    pass
+
+def freeCameraRotatePitch(float):
+    pass
+
+def freeCameraRotateHeading(float):
+    pass
+
+def freeCameraRotateRoll(float):
+    pass
+
+def sendChatMessage(string_msg):
+    pass
+
+def addOnChatMessageListener(ac_window_id, call_back_function):
+    pass
+
