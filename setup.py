9fd4cd17c2c138758ad9802ec7d0af9607f69034
Kirby Rs <andkirby@gmail.com>
2023-03-23 00:30:32 +0200

    Added missed methods found in SimHub mod for AC.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.4.0'
+VERSION = '0.4.1'
 DESCRIPTION = 'Assetto Corsa stubs library.'
 LONG_DESCRIPTION = 'Assetto Corsa stubs library for "ac" object. Useful for autocompletion in IDE during development.'
 
